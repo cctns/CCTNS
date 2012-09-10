1f05d6e3a7193fae51fef84c88658c102b127222
cctns <cc7ns@live.in>
2012-09-10 14:48:34 -0700

    layout

---
+++ application/views/templates/footer.php
@@ -0,0 +1,25 @@
+        <footer>
+            <p>&copy; Company 2012</p>
+        </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<? echo base_url()?>_assets/bootstrap/js/jquery.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-transition.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-alert.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-modal.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-dropdown.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-scrollspy.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-tab.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-tooltip.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-popover.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-button.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-collapse.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-carousel.js"></script>
+    <script src="<? echo base_url()?>_assets/bootstrap/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,13 @@
+
+        <div class="span3 pull-left">
+          <img src="<? echo base_url()?>_assets/bootstrap/img/tlogo.jpg">
+        <hr>
+        <div class="span10">
+	        <ul class=" nav nav-pills nav-stacked">
+			  <li class="active"><a href="#">Home</a></li>
+			  <li class=""><a href="#">option1</a></li>
+			  <li class=""><a href="#">option2</a></li>
+			  <li class=""><a href="#">option3</a></li>
+			</ul>
+		</div>
+        </div>
