00bc39db413e5df1ae8af5d9aede123b2c33a568
cctns <cc7ns@live.in>
2012-09-19 02:40:32 +0530

    k10 - 19/9/12

--- application/views/auth/register_form.php
+++ application/views/auth/register_form.php
@@ -9,7 +9,7 @@
 			'value' => set_value('username'),
 			'maxlength'	=> $this->config->item('username_max_length', 'tank_auth'),
 			'size'	=> 30,
-			'class' => 'control-label',
+			
 			
 		);
 	}
@@ -65,72 +65,63 @@
 		'name' => 'register'
 		 );
 	$label=array(
-		'class' => ' control-label'
+		'class' => 'control-label'
 	);
 		echo form_open($this->uri->uri_string(),$form); ?>
 		<legend>Register !</legend>
 			 <?php if ($use_username) { ?>
 			<div class="control-group">
-				<?php echo form_label('Username', $username['id'],$username);?>
+				<?php echo form_label('Username', $username['id'],$label);?>
 				<div class="controls"> 
 					 <?php echo form_input($username); 
 					 echo form_error($username['name']); 
 					 echo isset($errors[$username['name']])?$errors[$username['name']]:'';
 					 } ?>
 				</div>
-				</div>
+			</div>
 			<div class="control-group">
 				<?php echo form_label('Email Address', $email['id'],$label); ?>
 				<div class="controls"> 
 					<?php echo form_input($email); 
 					 echo form_error($email['name']);  echo isset($errors[$email['name']])?$errors[$email['name']]:'';?> 
 				</div>
-				</div>
+			</div>
 			
-		<div class="control-group">
+			<div class="control-group">
 				<?php	echo form_label('Password', $password['id'],$label); ?>
 				<div class="controls"> 
 				 	<?php echo form_password($password); ?>
 					<?php  echo form_error($password['name']); ?>
 				</div>
-				</div>
-		<div class="control-group">	
-		<?php echo form_label('Confirm Password', $confirm_password['id'],$label); 
+			</div>
+			<div class="control-group">	
+				<?php echo form_label('Confirm Password', $confirm_password['id'],$label); 
 				 echo form_error($password['name']); ?>	
 				 <div class="controls"> 
 					 <?php echo form_password($confirm_password); 
 					 echo form_error($confirm_password['name']); ?>
 				</div>
-		</div>
-
-				<div class="control-group">
-					<?php echo form_label('Phone', $phone['id'],$label); ?>
-						<div class="controls"> 
-							<?php echo form_input($phone); 
-							 echo form_error($confirm_password['name']);?> 
-						</div>
+			</div>
+			<div class="control-group">
+				<?php echo form_label('Phone', $phone['id'],$label); ?>
+				<div class="controls"> 
+					<?php echo form_input($phone); 
+					 echo form_error($confirm_password['name']);?> 
 				</div>
-
-				 <div class="control-group">
-    					<label class="control-label" for="inputEmail">Email</label>
-    					<div class="controls">
-      						<input type="text" id="inputEmail" placeholder="Email">
-    					</div>
-  				</div>
-				
-		<div class="control-group">
+			</div>
+			<div class="control-group">
 				<?php echo form_label('Address', $address['id'],$label); ?>
 				<div class="controls"> 
 					<?php echo form_textarea($address); 
-					 echo form_error($address['name']); ?>
+				 	echo form_error($address['name']); ?>
 				</div>
+			</div>
+			<div class="control-group">
+				<div class="form-actions">
+					<?php echo form_submit($btn, 'Register'); 
+		 			echo form_close(); ?>
+		 			<button type="button" class="btn">Cancel</button>
 				</div>
-				<div class="control-group">
-		<div class="form-actions">
-				<?php echo form_submit($btn, 'Register'); 
-		 	echo form_close(); ?>
-		 <button type="button" class="btn">Cancel</button>
-		</div>
-		</div>
+			</div>
 </div>
 
